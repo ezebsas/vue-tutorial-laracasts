39eb93b804d1e72159e6761252e2ec42cb4b1f35
Unknown <mollinedoezequiel@gmail.com>
2018-02-15 18:29:20 -0300

    componentes adentro de componentes

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+Vue.component('task-list', {
+  template: `
+    <div>
+      <task v-for='task in tasks'>{{task.description}}</task>
+    </div>
+  `,
+  data(){
+    return {
+      tasks: [
+        {description: 'Go to the store', completed: true},
+        {description: 'Buy', completed: false},
+        {description: 'Work', completed: false},
+        {description: 'Clean', completed: true},
+      ]
+    };
+  }
+});
+
 Vue.component('task', {
   template: '<li><slot></slot></li>'
 });
