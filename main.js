c4ca815840e82033079c17bc3a76726f827e8f50
Unknown <mollinedoezequiel@gmail.com>
2018-02-16 12:18:10 -0300

    practica componentes mensajes 1

--- main.js
+++ main.js
@@ -1,23 +1,22 @@
-Vue.component('task-list', {
-  template: `
-    <div>
-      <task v-for='task in tasks'>{{task.description}}</task>
-    </div>
-  `,
-  data(){
-    return {
-      tasks: [
-        {description: 'Go to the store', completed: true},
-        {description: 'Buy', completed: false},
-        {description: 'Work', completed: false},
-        {description: 'Clean', completed: true},
-      ]
-    };
-  }
-});
+Vue.component('message', {
+  props: ['title', 'body'],
 
-Vue.component('task', {
-  template: '<li><slot></slot></li>'
+  data() {
+    return {
+        isVisible: true
+    };
+  },
+  template: `
+  <article class="message" v-show="isVisible">
+    <div class="message-header">
+      {{title}}
+      <button type="button" @click="isVisible = false">X</button>
+    </div>
+    <div class="message-body">
+      {{body}}
+    </div>
+  </article>
+  `
 });
 
 
